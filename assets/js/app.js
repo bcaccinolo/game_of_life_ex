8294ade8562c5367e451a3fb4bc1095f607fe22c
Benoit Caccinolo <bcaccinolo@synbioz.com>
2019-07-10 15:53:00 +0200

    Addition React

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,9 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 import socket from "./socket"
+
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Root from './components';
+
---
+++ assets/js/components.js
@@ -0,0 +1,10 @@
+
+import * as React from 'react';
+
+class Root extends React.Component {
+  render() {
+    return "<h1>Hello world!</h1>"
+  }
+}
+
+export default Root;
