3ff0865b11ebc1d737b03ae4846b8d97a80a62fd
Benoit Caccinolo <bcaccinolo@synbioz.com>
2019-07-10 14:01:02 +0200

    send empty message

--- lib/phx_game_of_life_web/channels/room_channel.ex
+++ lib/phx_game_of_life_web/channels/room_channel.ex
@@ -8,4 +8,9 @@ defmodule PhxGameOfLifeWeb.RoomChannel do
     {:ok, socket}
   end
 
+  # client asks for their current rank, push sent directly as a new event.
+  def handle_in("new_msg", payload, socket) do
+    IO.puts("New message from client")
+    {:noreply, socket}
+  end
 end
