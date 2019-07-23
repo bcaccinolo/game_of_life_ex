d2f2da056dd349ae74d20530cc086674143faf7d
Benoit Caccinolo <bcaccinolo@synbioz.com>
2019-07-23 15:33:21 +0200

    cleaning

--- test/game_of_life_core/state_builder_test.exs
+++ test/game_of_life_core/state_builder_test.exs
@@ -7,12 +7,12 @@ defmodule StateBuilderTest do
   require IEx
 
   test "build_state_from_value" do
-    {:ok, pid} = GameOfLifeCore.StateBuilder.build_state_from_value(1)
+    {:ok, pid} = StateBuilder.build_state_from_value(1)
     assert is_pid(pid)
   end
 
   test "build_state_line" do
-    res = GameOfLifeCore.StateBuilder.build_state_line([1,0,1])
+    res = StateBuilder.build_state_line([1,0,1])
     assert res |> Tuple.to_list |> length == 3
     assert is_pid(elem(res, 1))
   end
@@ -21,18 +21,18 @@ defmodule StateBuilderTest do
     state = [[1, 0, 0, 1],
              [1, 1, 0, 1],
              [1, 0, 0, 1]]
-    res = GameOfLifeCore.StateBuilder.build_state(state)
+    res = StateBuilder.build_state(state)
     assert res |> Tuple.to_list |> length == 3
     assert is_tuple(res)
   end
 
   test "random_state_line" do
-    res = GameOfLifeCore.StateBuilder.random_state_line(3)
+    res = StateBuilder.random_state_line(3)
     assert length(res) == 3
   end
 
   test "random_state" do
-    res = [h | t] = GameOfLifeCore.StateBuilder.random_state(50,25)
+    res = [h | t] = StateBuilder.random_state(50,25)
     assert length(res) == 50
     assert length(h) == 25
   end
