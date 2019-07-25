c836d444db7e941dc324c3e391b061fa0aef6022
Benoit Caccinolo <bcaccinolo@synbioz.com>
2019-07-25 10:37:34 +0200

    update State module

--- lib/game_of_life_core/state.ex
+++ lib/game_of_life_core/state.ex
@@ -12,20 +12,21 @@ defmodule GameOfLifeCore.State do
 
   @doc """
   Returns the element present at the given coordinates.
-  It manipulates the list as a 2 dimensions matrix.
+  Coordinates are passed cause we manipulate the list as a 2 dimensions matrix.
+  If coordinates are out of scope, `:out_of_boundaries` is returned.
   """
-  def get(state, line, col, x, y) do
-    Enum.at(state, position(line, col, x, y))
-  end
+  def get(_state, _line, _col, 0, _y), do: :out_of_boundaries
+  def get(_state, _line, _col, _x, 0), do: :out_of_boundaries
+  def get(state, line, col, x, y), do: Enum.at(state, position(line, col, x, y))
 
   @doc """
   Returns the index of the value in the list.
 
   If the coordinates are out of boundaries, 0 is returned.
   """
-  def position(_line, _col, x, _y) when x < 1, do: 0
-  def position(line, _col, x, _y) when x > line, do: 0
-  def position(_line, _col, _x, y) when y < 1, do: 0
-  def position(_line, col, _x, y) when y > col, do: 0
+  def position(_line, _col, x, _y) when x < 1, do: -1
+  def position(line, _col, x, _y) when x > line, do: -1
+  def position(_line, _col, _x, y) when y < 1, do: -1
+  def position(_line, col, _x, y) when y > col, do: -1
   def position(_line, col, x, y), do: col * (x - 1) + y - 1
 end
