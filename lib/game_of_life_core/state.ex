a94f29259d4b1be577472202f568675f1668a87e
Benoit Caccinolo <bcaccinolo@synbioz.com>
2019-07-25 13:18:48 +0200

    cleaning tests

--- lib/game_of_life_core/state.ex
+++ lib/game_of_life_core/state.ex
@@ -1,15 +1,4 @@
 defmodule GameOfLifeCore.State do
-  # Callback invoked by `use`.
-  #
-  # For now it returns a quoted expression that
-  # imports the module itself into the user code.
-  @doc false
-  defmacro __using__(_opts) do
-    quote do
-      import GameOfLifeCore.State
-    end
-  end
-
   @doc """
   Returns the element present at the given coordinates.
   Coordinates are passed cause we manipulate the list as a 2 dimensions matrix.
@@ -29,4 +18,10 @@ defmodule GameOfLifeCore.State do
   def position(_line, _col, _x, y) when y < 1, do: -1
   def position(_line, col, _x, y) when y > col, do: -1
   def position(_line, col, x, y), do: col * (x - 1) + y - 1
+
+  @doc """
+  From an index of the list, returns the corresponding coordinates of the 2 dimensions matrix.
+  """
+  def coordinates(index, line, col) do
+  end
 end
--- test/game_of_life_core/state_test.exs
+++ test/game_of_life_core/state_test.exs
@@ -1,29 +1,34 @@
 defmodule StateTest do
   use ExUnit.Case
-  use GameOfLifeCore.State
+
+  alias GameOfLifeCore.State
+
   doctest GameOfLifeCore
+
+  test "coordinates" do
+  end
 
   test "position" do
     {line, col} = {5, 4}
 
-    assert GameOfLifeCore.State.position(line, col, 1, 1) == 0
-    assert GameOfLifeCore.State.position(line, col, 3, 3) == 10
-    assert GameOfLifeCore.State.position(line, col, 5, 4) == 19
-    assert GameOfLifeCore.State.position(line, col, 10, 10) == -1
+    assert State.position(line, col, 1, 1) == 0
+    assert State.position(line, col, 3, 3) == 10
+    assert State.position(line, col, 5, 4) == 19
+    assert State.position(line, col, 10, 10) == -1
   end
 
   test "get" do
     {state, line, col} = {[0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0], 5, 4}
 
     # Corners
-    assert GameOfLifeCore.State.get(state, line, col, 1, 1) == 0
-    assert GameOfLifeCore.State.get(state, line, col, 1, 4) == 0
-    assert GameOfLifeCore.State.get(state, line, col, 5, 1) == 0
-    assert GameOfLifeCore.State.get(state, line, col, 5, 4) == 0
+    assert State.get(state, line, col, 1, 1) == 0
+    assert State.get(state, line, col, 1, 4) == 0
+    assert State.get(state, line, col, 5, 1) == 0
+    assert State.get(state, line, col, 5, 4) == 0
 
     # Random values
-    assert GameOfLifeCore.State.get(state, line, col, 3, 2) == 1
-    assert GameOfLifeCore.State.get(state, line, col, 4, 3) == 1
-    assert GameOfLifeCore.State.get(state, line, col, 2, 2) == 0
+    assert State.get(state, line, col, 3, 2) == 1
+    assert State.get(state, line, col, 4, 3) == 1
+    assert State.get(state, line, col, 2, 2) == 0
   end
 end
