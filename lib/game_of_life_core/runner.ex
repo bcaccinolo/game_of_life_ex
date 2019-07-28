0cce3bc4cfb52bda3778555ebaeca9e3f7dc8e7b
Benoit Caccinolo <bcaccinolo@synbioz.com>
2019-07-28 21:08:13 +0200

    Runner is the public interface

--- lib/game_of_life_core/runner.ex
+++ lib/game_of_life_core/runner.ex
@@ -1,5 +1,13 @@
 defmodule GameOfLifeCore.Runner do
-  alias GameOfLifeCore.{StateAgent, GolServer}
+  alias GameOfLifeCore.{StateAgent, StateBuilder, GolServer}
+
+  @doc """
+  Build a random board and start the StateAgent server
+  """
+  def build_random_board(line, col) do
+    StateBuilder.random_state(line, col)
+    |> StateAgent.start_link()
+  end
 
   @doc """
   Do one Game of Life iteration on the existing state.
@@ -12,5 +20,12 @@ defmodule GameOfLifeCore.Runner do
     |> Enum.map(fn index -> StateAgent.environment_and_cell_by_index(index) end)
     |> Enum.map(fn {env, pid} -> Task.async(fn -> GolServer.calculate(pid, env) end) end)
     |> Enum.each(fn task -> Task.await(task) end)
+  end
+
+  @doc """
+  Return the board state in string format
+  """
+  def string_state do
+    StateAgent.to_s
   end
 end
